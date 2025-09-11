cedc03137f8e936589b4cdbefdf4ec32e8dfdd80
willgarr985 <fearspi@gmail.com>
2025-09-11 15:27:24 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,37 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
+    createPlatform(100,0, 10,500,"white");
+createPlatform(1350, 400, 50, 50, "purple");
+createPlatform(200, 650, 100, 200,"white");
+createPlatform(500, 550, 150, 300,"purple");
+createPlatform(800,250,15,800,"white");
+createPlatform(100,450,200,10,"purple");
+createPlatform(400,325,200,10,"white");
+createPlatform(1000,0, 10,500,"purple");
+createPlatform(1200,625,200,10,"white");
+createPlatform(1000,500,200,10,"purple");
 
 
 
     // TODO 3 - Create Collectables
+  createCollectable("steve", 200, 170, 0.5, 0.7);
+  createCollectable("steve", 800, 170, 0.5, 0.7);
+  createCollectable("steve", 1350, 170, 0.5, 0.7);
 
 
 
     
     // TODO 4 - Create Cannons
-
+ createCannon("top", 200, 1000);
+createCannon("right", 300, 2000);
+createCannon("bottom",700,1000);
+createCannon("top",1200,1000);
+createCannon("right",600,1500)
 
     
     
